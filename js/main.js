dfebde333f0cd0359f3620414a904c13c989793f
Emily Sarah Kern <emily.kern28@gmail.com>
2018-11-30 22:21:38 -0500

    made changes as noted in the code review

--- js/main.js
+++ js/main.js
@@ -172,6 +172,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + " Restuarant";
   li.append(image);
 
   const name = document.createElement('h1');
